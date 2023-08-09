740e3e8cde3d800884e0556c02ffcc7c49faf65e
FaridaFatali <faridafatali.91@gmail.com>
2023-08-09 14:58:17 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Practice: <a href="https://www.youtube.com/watch?v=rA7tfvpkw0I">...</a>
+ * @author Farida Fatali
+ * Making Tic-Tac-Toe
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+    }
+}
